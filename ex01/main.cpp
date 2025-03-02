048fbeb199ed63965e8143fd041ddfd54bb4b868
aes-sarg <aes-sarg@e2r8p4.1337.ma>
2025-03-02 18:08:36 +0000

    ready

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,7 +7,14 @@ int main(int argc, char *argv[])
         std::cerr << "Error: Invalid number of arguments" << std::endl;
         return 1;
     }
-
+    try
+    {
     RPN::evaluate(argv[1]);
+
+    }
+    catch(std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
     return 0;
 }
